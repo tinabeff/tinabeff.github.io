0e129ab59abc42f9f614e331d5c68479e6692ef3
tinabeff <tinabeff@gmail.com>
2014-10-18 03:43:50 +0000

    random buuble

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -1,38 +1,32 @@
-/**
-* Created with tinabeff.github.io.
-* User: tinabeff
-* Date: 2014-10-18
-* Time: 03:18 AM
-* To change this template use Tools | Templates.
-*/
-
 (function($) {
 	$.ajax({
 		dataType: 'json',
 		url: 'http://api.openweathermap.org/data/2.5/weather?q=Taipei',
 	    success: function(response, jqXHR, textStatus) {
+            var data = [];
+            var obj = {};
+            
+            obj.temp = response.main.temp;
+            obj.humidity = response.main.humidity;
+            
+            // Weather Icons
+            switch (response.weather[0].main) {
+                case "Clouds": obj.weatherIcon = "wi-day-cloudy";
+            }
+            
+            // Celsius
+            obj.celsius = response.main.temp - 273.15;
+            
+            
+            data.push(obj);
+            
 			$('#postTemplate')
-				.tmpl(response.posts)
+				.tmpl(data)
 				.appendTo('#content');
+            
+            $('#weather-icon').addClass('wi-day-cloudy');
 	    },
 	    complete: function(jqXHR, textStatus) {
-			$('[id=subject]').each(function(index) {
-				var me = $(this);
-				
-				me.on('click', function() {
-					var id = me.data('id');
-
-					$.ajax({
-						dataType: 'json',
-						url: 'http://localhost:3000/1/post/' + id,
-						success: function(response, jqXHR, textStatus) {
-							$('#content').html('<h2>' 
-								+ response.post.content 
-								+ '</h2>');
-						},
-					});
-				});
-			});
 	    }
 	});
 }) ($);
