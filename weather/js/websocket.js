d96a57451a327e801c31b33f8c05c9f5c9b181f4
tinabeff <tinabeff@gmail.com>
2014-10-19 03:39:10 +0000

    random bubble

--- weather/js/websocket.js
+++ weather/js/websocket.js
@@ -1,18 +1,24 @@
-(function(){  
-    $.fn.createWebSocket=function(){    
+
+(function() {
+    $.fn.createWebSocket = function() {
         var ws = new WebSocket("ws://zero-manila.codio.io:3000/", "echo-protocol"); 
         ws.onopen = function(evt) {
-            $('[data-status]').addClass('hide');
-            $('[data-status=success]').removeClass('hide');
+             $('[data-status]').addClass('hide');
+             $('[data-status=success]').removeClass('hide');
         };
         ws.onclose = function(evt) {
-
+             $('[data-status]').addClass('hide');
+             $('[data-status=close]').removeClass('hide'); 
         };
         ws.onerror = function(evt) {
-            $('[data-status]').addClass('hide');
-            $('[data-status=error]').removeClass('hide');
+             $('[data-status]').addClass('hide');
+             $('[data-status=error]').removeClass('hide');
         };
         ws.onmessage = function(evt) {
-        };
-    }
+
+        };    
+    }; 
 }) ();
+
+
+
