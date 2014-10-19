4f0fccc23a5a712e58274c6b3b43e702e9f19c5c
tinabeff <tinabeff@gmail.com>
2014-10-19 07:02:33 +0000

    random bubble

--- weather/js/weather.js
+++ weather/js/weather.js
@@ -1,6 +1,12 @@
 (function($) {
     $('#send').on('click',function(){
-       alert("you got me") ;
+        event.preventDefault();//有防止其表單在送出時會閃一下
+        var msg=$('msg').val();
+        $.ajax({
+            dataType: 'json',
+                type:'post',
+            url: 'http://zero-manila.codio.io:3000/send/'+msg
+	    });
     });
 	$.ajax({
 		dataType: 'json',
