6d498afffa905483c730fbb3e30559a450ed4d9c
tinabeff <tinabeff@gmail.com>
2014-10-19 03:36:56 +0000

    random bubble

--- weather/js/chat.js
+++ weather/js/chat.js
@@ -1,18 +1,27 @@
+
 (function($) {
 	$.ajax({
 		dataType: 'json',
 		url: 'http://zero-manila.codio.io:3000/start',
 	    success: function(response, jqXHR, textStatus) {
-            //for (i = 0; i < response.data.length; i++) {
-            //    response.data[i].timestamp = moment(response.data[i].timestamp).fromNow();
-            //}
+            //Dirty:
+            // for (i = 0; i < response.data.length; i++) {
+            //     response.data[i].timestamp = moment(response.data[i].timestamp).fromNow();
+            // }
             
 			$('#chatTemplate')
 				.tmpl(response.data)
 				.appendTo('#content');  
 	    },
 	    complete: function(jqXHR, textStatus) {
-           $(document).createWebSocket();
+            // SPA Principle: MVC Architecture
+            //  - Modify View instead of Model
+            $('.timestamp').each(function() {
+                var me = $(this);
+                var timestamp = me.html();
+
+                me.html(moment(timestamp).fromNow());
+            });
 	    }
 	});
 }) ($);
